0bb982583a3fe1b2cfd939c5f67bc8e49da953da
psicoxX <161771219+psicoxX@users.noreply.github.com>
2024-06-24 00:42:17 -0600

    Actualizar Registro.php

--- proyectofinal/conexionbd/Registro.php
+++ proyectofinal/conexionbd/Registro.php
@@ -16,6 +16,7 @@ try {
     $contraseña = $_POST['contrasena'];
     //PERFILES ES LA TABLA Y SELECCIONA LA PRIMERA COLUMNA QUE SALE
 //GENERALMETE ES ID DESPUES CORREO Y SEGUIDO DE PASSWORS
+//EN ESTE CASO MI PRIMER COLUMNA ES EL CORREO
     $stmt = $pdo->prepare("SELECT * FROM perfiles WHERE correo = ?");
     $stmt->execute([$correo]);
     $usuario_existente = $stmt->fetch();
@@ -27,6 +28,8 @@ try {
         
        
     } else {
+//AQJU LO QUE HACEMOS ES PREPARAR LA BASE
+//PARA DESPUES BUSCAR EN MI CASO EL ORDEN ES //CORREO Y DESPUES CONTRASEÑA
         $stmt = $pdo->prepare("INSERT INTO perfiles (correo, contrasena) VALUES (?, ?)");
         $stmt->execute([$correo, $contraseña]);
         echo "Registro exitoso. Puedes iniciar sesión <a href='../index.php'>aquí</a>.";
@@ -35,5 +38,12 @@ try {
 } catch (PDOException $e) {
     echo "Error al registrar usuario: " . $e->getMessage();
 }
+
+//ENTONCES RECAPITULANDO
+//MI BASE SE LLAMA NIGGER
+//MI TABLA SE LLAMA PERFILES Y ES DE DONDE VOY A MANDAR MIS REGISTROS
+/MI PRIMERA COLUMNA ES CORREO/USUARIO
+Y SERÍA MI PRIMER COLUMNA CORREO Y SEGUIDO DE CONTRASEÑA
+
 
 ?>
